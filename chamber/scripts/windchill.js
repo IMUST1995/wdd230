8712f25aa6b643657d0c2140913450ffc3e0dd2b
IMUST1995 <zeirbraidi@gmail.com>
2022-10-25 00:30:14 -0600

    weather

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,25 @@
+const windSpeedHolder =  document.querySelector('.windSpeedHolder')
+const windChillHolder = document.querySelector('.windChillHolder')
+
+function windChillFahrenheit(temperature, windSpeed){
+  windChill = 35.74 + 0.6215 * temperature - 35.75 * windSpeed ** 0.16 + 0.4275 * temperature * windSpeed ** 0.16
+  windChill = windChill.toFixed(2)
+  return windChill
+};
+
+const temperature = 50.0
+const windSpeed = 3.0
+
+windSpeedHolder.innerHTML = windSpeed
+if(temperature <= 50.0 && windSpeed >= 3.0){
+    const currentWindChillFahrenheit = windChillFahrenheit(temperature, windSpeed)
+    windChillHolder.innerHTML = currentWindChillFahrenheit
+    windChillHolder.innerHTML += ' mi/h'
+}
+else{
+    windChillHolder.innerHTML = 'N/A'
+}
+
+
+
+console.log(currentWindChillFahrenheit)
