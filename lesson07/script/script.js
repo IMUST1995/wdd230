f288169f9138541c42f3904d2d2284d76622aac3
IMUST1995 <zeirbraidi@gmail.com>
2022-10-27 23:23:31 -0600

    lazy loading

--- lesson07/script/script.js
+++ lesson07/script/script.js
@@ -1,32 +1,4 @@
-/* const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c4fc425f2bdf0be4e46d3bf001bef301&page=1'
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=c4fc425f2bdf0be4e46d3bf001bef301&query="'
-
-const main = document.getElementById('main')
-
-getMovier(API_URL)
-
-async function getMovier(url){
-    const res = await fetch(url)
-    const data = await res.json()
-    showMovies(data.results)
-}
-
-function showMovies(movies){
-    main.innerHTML = ''
-    movies.forEach(movie => {
-        const {title, poster_path} = movie
-
-        const movieElement = document.createElement('div')
-        movieElement.classList.add('movie')
-        movieElement.innerHTML = `
-            <img src="${IMG_PATH + poster_path}" alt="${title}" loading="lazy">`
-        main.appendChild(movieElement)
-    });
-    
-} */
-
-const images = document.querySelectorAll("[data-src]");
+/* const images = document.querySelectorAll("[data-src]");
 
 
 function preloadImage(img) {
@@ -35,20 +7,10 @@ function preloadImage(img) {
         return;
     }
     img.src = src
-/*     img.style.filter = 'blur(.2em)' */
-}
-/* .movie img[data-src] {
-    filter: blur(0.2em);
-  }
-.movie img {
-filter: blur(0em);
-transition: filter 0.5s;
-} */
-
 
 const options = {
     threshold: 0,
-
+    rootMargin: '0px 0px 10px 0px' 
 };
 
 const imgObserver = new IntersectionObserver((entries, imgObserver) => {
@@ -64,26 +26,36 @@ const imgObserver = new IntersectionObserver((entries, imgObserver) => {
 
 images.forEach(e =>{
     imgObserver.observe(e)
-})
-/* function lazyLoading(){
-    const imgs = document.querySelectorAll('[data-src]');
-console.log(imgs)
-function intersection(entries, observer) {
-    entries.forEach(entry => {
-        consolee.log('intersection', entry.target)
-    })
-};
-//defect valuer =>
-const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0
-};
-//defining observer
-const observer = new IntersectionObserver(intersection, options);
+}) */
 
-img.forEach(e => {
-    observer.observe(e)
-});
+
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+console.log(imagesToLoad)
+const imgOptions = {
+  rootMargin: '0px 0px 10px 0px',
+  threshold: 1
+};
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};  
+if ("IntersectionObserver" in window) {
+  const imgObserver = new IntersectionObserver((items, imgObserver) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        imgObserver.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+  imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
 }
-lazyLoading() */
